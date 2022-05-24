25befa4915704ff77babc80a2f78f4d2af15f582
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-05-24 09:28:06 +0200

    tweet

---
+++ src/components/Actions.js
@@ -0,0 +1,39 @@
+function Tweet({ tweet }) {
+    return (
+      <div className="tweet">
+        <img
+          src={tweet.user.image}
+          className="profile"
+          alt="profile"
+        />
+  
+        <div className="body">
+          <div className="top">
+            <span className="user">
+              <span className="name">{tweet.user.name}</span>
+              <span className="handle">{tweet.user.handle}</span>
+            </span>
+  
+            <span className="timestamp">{tweet.timestamp}</span>
+  
+          </div>
+  
+          <p className="message">
+            {tweet.message}
+          </p>
+  
+          <div className="actions">
+            {/* Font Awesome icons */}
+            <i class="far fa-comment"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="far fa-heart"></i>
+            <i class="fas fa-share"></i>
+          </div>
+        </div>
+  
+        <i class="fas fa-ellipsis-h"></i>
+      </div>
+    );
+  }
+  
+  export default Tweet;
